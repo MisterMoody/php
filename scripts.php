d52d53d33b9cea572e622879a9dccc50d0faf36c
Mister <principal@mistermoody.com>
2017-10-29 17:05:48 -0400

    Add Page + Install Carousel for Scripts

---
+++ scripts.php
@@ -0,0 +1,230 @@
+<?php
+/* dateTime VAR*/
+$date = date_default_timezone_set('EST');
+
+?>
+
+<!-- ********************************************************* -->
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A Self-Directing Guidebook  to Building an Application Using PHP">
+    <meta name="keywords" content="Mister Moody, Web Design, PHP, CSS, HTML">
+    <meta name="author" content="Ray Moody">
+    <meta name="twitter:title" content="@Mister_Moody" />        
+    <meta name="twitter:site" content="http://mistermoody.com" />
+    <!-- Browser Tab Icon -->
+    <!--<link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">--> 
+    <!-- TITLE -->
+    <title>PHP 101</title>
+    <!-- *********** CSS Implementations ************ -->
+    <!-- BOOTSTRAP *CDN* Compiled & Minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- CUSTOM Styles -->   
+    <link rel="stylesheet" href="styles.css">        
+    <!-- Font Awesome ICONS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- FONTS -->       
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    
+	  <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway" rel="stylesheet">
+  </head>
+  
+  <body>
+    <!-- ********************************************************* -->
+    <!-- *************** <NAVIGATION> CONTAINER[S] *************** -->
+    <!-- ********************************************************* -->
+    <nav class="navbar sticky-top navbar-expand-md navbar-toggleable-sm navbar-light"> 
+      <a class="navbar-brand"><span class="font-weight-bold navPhone pl-1">PHP 101</span>
+        <!--<span class="font-weight-bold navPhone pl-1"><i class="fa fa-phone d-lg-inline-block"></i> +1.502.558.9240 </span>-->
+        <!--<img src="..." width="30" height="30" class="d-inline-block align-top" alt="">-->
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto"> <!-- Use "ml-auto" for Right Side Alignment-->
+          <li class="nav-item active text-right">
+            <a class="nav-link" href="index.html"><i class="fa fa-home d-none d-lg-inline-block"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" src="#" href="syntax.php" alt="PHP Snytax">Syntax</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" src="#" href="scripts.php" alt="PHP Scripts">Scripts</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Database</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">aaa</a>
+              <a class="dropdown-item" href="#">bbb</a>
+            </div>
+          </li>
+          <!--<li class="nav-item">
+            <a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">502WEST</a>
+              <a class="dropdown-item" href="#">CLIL</a>
+            </div>
+          </li>
+          --> <!-- HIDDEN Nav Items -->
+        </ul>
+      </div>
+     </nav>
+    <!-- ********************************************************* -->
+    <!-- ********************************************************* -->
+    
+    
+    <!-- ********************************************************* -->
+    <!-- *************** <MAIN> CONTAINER ************************ -->
+    <!-- ********************************************************* -->
+    <!-- 1-COL -->
+    <!--<div class="grid">
+      <div class="col-12">
+        <h3>COLUMN 1 OF 1</h3>
+      </div>
+      <div class="col-12">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis laoreet nunc. Donec ut ligula eget dolor pellentesque vestibulum vel in elit. Morbi posuere aliquam diam sit amet sagittis. Nulla facilisi. Cras a malesuada leo, id malesuada neque. Phasellus vestibulum venenatis velit non mattis. Nam lobortis velit nec sollicitudin cursus. Nam maximus consectetur iaculis. Pellentesque et ultricies magna.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed non finibus nibh, eu rhoncus purus. Ut pulvinar quam nec tristique interdum. In libero purus, volutpat ut lobortis vitae, dapibus sit amet arcu. Morbi bibendum magna eu.</p>
+      </div>
+      <div class="col-12"></div>
+    </div>-->
+    <!-- -->
+    <div class="container">
+      
+      <!-- -->
+      <!-- -->
+      <!-- Scripts -->
+      <div class="row">
+        <!-- -->
+        <div class="col-6">
+          <h1 class="text-center font-weight-bold mb-2">PHP Scripts</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis laoreet nunc. Donec ut ligula eget dolor pellentesque vestibulum vel in elit. Morbi posuere aliquam diam sit amet sagittis. Nulla facilisi. Cras a malesuada leo, id malesuada neque. Phasellus vestibulum venenatis velit non mattis. Nam lobortis velit nec sollicitudin cursus. Nam maximus consectetur iaculis. Pellentesque et ultricies magna.</p>
+          <p><a href="https://www.w3schools.com/php/php_examples.asp">PHP Examples</a></p>
+        </div>
+        <!-- -->
+        <div id="carouselExampleIndicators" class="carousel slide col-6" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://via.placeholder.com/250x150" alt="First slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Test</h3>
+                <p>......</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://via.placeholder.com/250x150" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Test</h3>
+                <p>......</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://via.placeholder.com/250x150" alt="Third slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Test</h3>
+                <p>......</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      <!-- -->
+      <!-- -->
+      
+      
+      
+      
+      <!-- -->
+      <!-- -->
+      
+      
+      <!-- RESOURCES -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h4>Resources</h4>
+          <ol>
+            <li>111 <a href="">xxx</a></li>
+            <li>111 <a href="">xxx</a></li>
+          </ol>
+        </div>
+      </div>
+      <!-- -->
+    </div>
+    
+    <!-- -->
+    <!-- -->
+    <!-- Version Control -->
+    <!--<div class="grid">
+      <div class="col-12">
+        <h3 id="vc">Version Control</h3>
+      </div>
+      <div class="col-12">
+        <ol>
+          <li>Create the Repo</li>
+          <p class="notes">From your account, Click <span class="highlight">New repository</span>.  Give the repo a name and brief description of the project objective. You want to <i>Initialize the Repo with a README</i> only if you are building a code from scratch, which is exactly what should be done here.</p>
+          
+          <li>Clone Repo to Computer</li>
+          <p class="notes">Cloning the repo is a multi-step process. First, from the repo click the 'Clone or Download' button. Next, <span class="highlight">Copy</span> the <span class="highlight">https://github.com/User/project.git</span> link.</p>
+          <p class="notes">Now, open the <span class="highlight">Control Panel</span> and access the <span class="highlight">C:\xampp\htodcs folder</span>. [Use the 'cd.. / cd ' commands to move down and up within the C:\ directory, respectively.]</p>
+          <p class="notes">Type <span class="highlight">git clone</span> then paste the repo link in the format provided above.</p>
+          
+          <li>Build the Project</li>
+          <p class="notes">If you already have files to build upon, simply copy and paste them into the repo folder.  Otherwise, create an index.html file and add it to the repo folder. Once you have done this, type <span class="highlight">git status</span>, which is a command that will allow you to <i>view the status of all project files</i>. Notice how the files are labled <span class="highlight">untracked</span>? That means that the file has not been uploaded from the computer to the Git repo online. Upload any file from the console using three simple commands: <span class="highlight">Add</span>, <span class="highlight">Commit</span> and <span class="highlight">Push</span>. Those commands are as follow:</p>
+          <ol type="a">
+            <li class="notes">git add fileName.extension</li>
+            <li class="notes">git commit -m "Changes Made to fileName.ext"</li>
+            <li class="notes">git push -u origin master</li>
+          </ol>
+          <p class="notes">That's it! Follow these three commands to modify any file in the future.  As well, there are several other commands that can be useful throughout your workflow such as <span class="highlight">Removing Files</span> or <span class="highlight">Cleaning the repo</span>, and this <span><a href="http://ndpsoftware.com/git-cheatsheet.html">Git Cheatsheet</a></span> can show you how!</p>
+        </ol>
+      </div>
+    </div>-->
+    <!-- -->
+    <!-- -->
+
+    <!-- ************ --> <!-- Use a Font-Awesome heart 'fa fa-heart' -->
+    <!-- ** FOOTER ** -->
+    <div class="container">
+      <div class="row no-gutters clearfix">
+        <div class="col-6">
+          <h6 class="text-left"><small>&#169;<?php echo date('Y'); ?></small> Made with <span>&#9829;</span> by Mister Moody.</h6>
+        </div>
+        <div class="col-6">
+          <h6 class="text-right"><?php echo date("D d M "); echo date(" g:i a T"); ?></h6>
+        </div>
+      </div>
+    </div>
+    <!-- END of FOOTER --> 
+    <!-- ************ -->
+    
+    <!-- *********************************************************************** -->
+    <!-- *********************************************************************** -->
+    <!-- ** jQuery, Popper & BOOTSTRAP *CDN* Compiled and Minified JavaScript ************ -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <!-- ********************************************************************************* -->
+    
+  </body>
+</html>
