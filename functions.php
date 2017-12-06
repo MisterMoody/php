3c74b4b30f77cfb897826c451c56c8d2e66cde9a
Mister <principal@mistermoody.com>
2017-12-06 18:21:53 -0500

    Internal Functions, String Functions, Array Functions

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ $date = date_default_timezone_set('EST');
             <li id=""><a id="fade" href="#">var Functions</a></li>
             <li id=""><a id="fade" href="#">Closures</a></li>
             <li id=""><a id="fade" href="#usingLogic">Returns</a></li>
-            <li id=""><a id="fade" href="#"><b>xxx</b></a></li>
+            <li id=""><a id="fade" href="#"><b>Internal Functions</b></a></li>
             <li id=""><a id="fade" href="#">Built-Ins</a></li>
             <li id=""><a id="fade" href="#">String Functions</a></li>
             <li id=""><a id="fade" href="#">Array Functions</a></li>
@@ -404,6 +404,147 @@ $date = date_default_timezone_set('EST');
               </div>
             </div>-->
           </div>
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <h3 id="implement" class="text-center font-weight-bold mb-2">Internal Functions</h3>
+          <p>PHP has a suite of <b>Internal Functions</b> to make coding easier. PHP clearly explains how to <a href="http://php.net/manual/en/about.prototypes.php">Read PHP Documentation</a> in order to <a href="http://php.net/manual/en/functions.internal.php">Use Internal Built-In Functions</a>. Reading a <i>Function Defintion</i> is simple so long as you understand <i>Data-Types</i>. The Function Definition describes the <i>Type of Value that is Returned</i>, returning an Integer Value and the Argument.</p>
+          <p>One such FD is the <b>String Function <code>strlen()</code></b> which will <code>Return the Length of a 'String'</code>. This function <code>int strlen (string $string)</code> expects the following:
+					<br>1st: <code>int</code> an <b>Integer</b> Value
+					<br>2nd: <code>strlen</code> a Function <b>Keyword</b>
+					<br>3rd: <code>&#40;</code>string <code>$string&#41;</code> an <i>Argument</i> of <b>String</b>-Type </p>
+          <p>There are also Functions that <i>Require <b>PHP Extensions</b> Compiled in</i>. For example, to use the MySQL Server Connection Function <code>mysqli_connect()</code>, PHP must be <i>compiled with</i> <a href="http://php.net/manual/en/ref.mysql.php">MySQL support</a>.  Learn more about the <a href="http://php.net/manual/en/function.mysqli-connect.php">mysqli_connect()</a> Function and look at the example below to <a href="http://php.net/manual/en/mysqlinfo.api.choosing.php">How to Utilize the API</a>.
+				  <code><br>$mysqli = new mysqli("example.com", "user", "password", "database");
+				  <br>$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+				  <br>$row = $result->fetch_assoc();
+				  <br>echo htmlentities($row['_message']);</code></p>
+				  <p>There are also Methods to <i>View Configuration, Compiled Modules and Any Extension used</i>. Try <i>Calling</i> <code>phpinfo()</code> or <code>get_loaded_extensions()</code> within a Function.</p>
+          <div class="alert alert-success" role="alert">
+					<b>ALWAYS</b> read the <a href="http://php.net/manual/en/configuration.php">Configuration</a>, <a href="http://php.net/manual/en/install.php">Installation</a>, and individual extension chapters, for information on <i>HOW to set up PHP</i>.
+        </div>
+				  <div class="alert alert-warning" role="alert">
+					<u>WARNING:</u> <b>From 2018, ALWAYS use the <code>mysqli_connect()</code> to "Connect PHP with MySQL DATABASE"!!!</b>
+					<br>The <code>mysql_connect()</code> Function was Deprecated in PHP 5.5.0 and Removed from PHP 7. -- As well, there are <i>Table-Type Transaction</i> issues using the <code>PDO()</code> Function.
+				</div>  
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <h3 id="implement" class="text-center font-weight-bold mb-2">String Functions</h3>
+          <p><b>Strings</b> are an important Data-Type that often requires that developers <i>Search for, Parse and Test Against</i>. Learn more about <a href="https://teamtreehouse.com/library/php-functions-2/php-internal-functions/php-string-functions">String Functions</a>.</p>
+          <div class="row mb-5">
+            <div class="col-12 mb-2">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title"><b>Return the Lenth</b> of a String</h4>
+						The <code>strlen()</code> Function <i>Returns the String Length</i>.
+						<br>
+						<code>$phrase = "We only hit what we aim for";
+						<br>$len = strlen($phrase);
+						<br>echo $len;</code>
+						<br>In this example, we first <i>Create a 'String-Variable' &amp; ASSIGN a 'String-Value'the</i> <code>$phrase = "We only hit what we aim for";</code>. Then we <i>Assign the 'strlen()' Function to a New Variable and PASS the 'String-Variable' as its 'Argument'</i> <code>$len = strlen($phrase);</code>. <br>Finally, we Call the 'String' <code>echo $len;</code> and this will output:
+						<?php $phrase = "We only hit what we aim for";
+						$len = strlen($phrase);
+						echo "<b>$len;</b>" ?>.
+            <p class="alert alert-dark card-text"><u>This example illustrates how to <i>Verify a given Password is at least 5 characters long</i></u>.
+							<br>
+							$pass = "abcdefghij";
+							<br>if (<code>strlen($pass)</code> < 5) { echo "Password MUST have at least 5 characters!";
+							<br>} else if (strlen($pass) >= 5) { echo "Your password is good!"; }
+						</p>
+            <!-- -->
+            <hr>
+            <h4 class="card-title"><b>Return a Part</b> of a String</h4>
+            <p>The <code>substr()</code> Function <i>Returns Part of a String</i>. This functions takes 2 arguments with an optional 3rd.
+						<br>
+						
+              
+						<br>In this example, we create a string-variable, assign it a string-value then assign the strlen() Function to a new variable and pass the string-variable as its argument. Then, we use the <code>echo substr($phrase, 0,5)</code> to Call the String. The 1st and 2nd arguments <i>target the string-variable</i> and <i>determines the starting position</i>. The optional 3rd argument <i>returns that # of characters</i> which is <code>5</code>. <br>Therefore, this code would output: <?php $phrase = "We only hit what we aim for"; 
+							$len = strlen($phrase);
+							echo "<b>substr($phrase, 0,5);</b>" ?></p>
+						<p class="alert alert-dark card-text"><u>This example illustrates how to <i>Return the Last 4 Digits of a CC</i></u>.
+							<br>
+							$card_number = "01 2345 6789";
+							<br>$digits = <code>substr($card_number, 7)</code>;
+          		<br>echo "The last 4 digits of your card are: **** " . $digits;
+						</p>
+                  <!-- -->
+                  <hr>
+                  <h4 class="card-title"><b>Return a Specified Character</b> of a String</h4>
+                  <p>The <code>strpos()</code> Function <i>Finds the position of the 1st occurrence of a substring in a string</i>.</p>
+                  <p>This example illustrates how to <i>Verify an '@' Exists within an Email</i></p>
+                  <p class="card-text text-muted">
+                    $email = "myEmail";
+                    <br>if (<code>strpos($email, "@")</code>) { echo "Good Email";
+                    <br>} else { echo "Bad email, please try again!>"; }
+                  </p>
+                  <!-- -->
+                </div>
+              </div>
+            </div>
+          </div>          
+          
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <!-- -->
+          <div class="col-12">
+            <h4>[3] Using <b>Array</b> Functions</h4>
+            <p></p>
+          </div>
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">The <code>array_keys()</code> Function</h4>
+                <p><b>Arrays</b> are useful Data-Types that allow developers to Store and Organize data.  Obviously, there are many ways to manage this data.</p>
+                The <b><code>array_keys()</code></b> Function <i><b>Returns all the Keys</b></i> or <i><b>a Subset of the Keys of an Array</b></i>. In the example below, we first (1) Create an 'Associative Array' & ASSIGN it 'Key-Value' pairs . (2) Then we use a 'foreach' Conditional Statement to Loop through the Array and PASS the 'Assoc-Array'as its 'Argument'. (3) Finally, we Call the 'Array'. 
+                <p class="card-text text-muted">$names = array('Ray' => 'Principal', 'James' => 'Entertainer');
+                <br>foreach<code>(array_keys($names)</code> as $name) {echo "Hello, $name";}
+                </p>This will output:
+                <?php $names = array('Ray' => 'Principal', 'James' => 'Entertainer');
+                foreach (array_keys($names) as $name) {echo "<b>Hello, $name. \n</b>";} ?>
+                <!-- -->
+                <br><br>
+                <p class="alert alert-dark card-text">xxx</p>
+                <!-- -->
+                <hr>
+                <h4 class="card-title">The <code>walk()</code> Function</h4>
+                The <code>array_walk()</code> Function <i><b>Returns the entire Key-Value Pairing within an Array</b></i>. Typically, Callback takes on two parameters. The array parameter's value being the first, and the key/index second. If callback needs to be working with the actual values of the array, specify the first parameter of callback as a reference. Then, any changes made to those elements will be made in the original array itself.
+                The example below uses the previously created <i>Associative-Array</i> then utilizes the <code>print_info()</code> Function like a conduit to <i>Pass all Array Key-Value Pairings</i> when Calling the <code>array_walk()</code> Function.
+                <p class="card-text text-muted">$names = array('Ray' => 'Principal', 'James' => 'Entertainer');
+                <br>function <code><b>print_info(</b></code>$value, $key<code><b>)</b></code>{
+      echo "<b>$key is a $value.</b>";}
+                <br><code><b>array_walk(</b></code>$names, <code>'print_info'</code><code><b>)</b></code>;
+                </p>This will output: <?php $names = array('Ray' => 'Principal', 'James' => 'Entertainer' ); function print_info($value, $key){echo "<b>$key is a $value.</b>\n";} array_walk($names, 'print_info'); ?>
+                <br>
+                <!-- -->
+                <hr>
+                <h4 class="card-title">The <code>array_comine()</code> Function</h4>
+                The <code>array_combine()</code> Function <i><b>Creates</b> a new Array of Key-Value Pairings by <b>Combining</b> Key-Value Pairings from Multiple Arrays</i>. <br>This Function will essentially <i><b>Create Two Arrays: one for Keys and the other for Values</b></i>. In the example below, (1) two Arrays are created, (2) a Variable is created and Assigned the <code>array_combine()</code> Function that takes the previously created Arrays as Arguments. (3) Finally, the <code>print_r()</code> Statement is used with the Variable holding the Function <code>$result</code>. 
+                <p class="card-text text-muted">
+                $name = array('Ray', 'James');
+                <br>$fav_food = array('tempura', 'meat');
+                <br>$result = <code>array_combine(</code>$name, $fav_food<code>);</code>
+                <br>print_r($result);
+                </p>This will output: <?php $name = array('Ray', 'James');
+    $fav_food = array('tempura', 'meat');
+    $result = array_combine($name, $fav_food);
+
+    print_r($result); ?>
+              </div>
+            </div>
+            <!-- -->
+          </div>
+          
+          
+          
           <h5>Data-Type Examples</h5>
           <div class="row">
             <div class="col-1">string</div>
@@ -472,6 +613,11 @@ $date = date_default_timezone_set('EST');
         </div>
         <!-- \End of MAIN-CONTENT -->
       </div>
+      
+      <!-- -->
+      <!-- -->
+      <!-- -->
+      <!-- -->
       <!-- \End of ROW-->
     </div>
     <!-- \End of CONTAINER-->
